47be62af3a48b497f105d4444fd67e126569ed15
Gerasimov Andrey <andrey.gerasimov2506@gmail.com>
2022-04-13 05:18:14 +0300

    Gerasimov Andrey

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FreeDrive - Прокат автомобилей в городе Пенза</title>
+        <!-- Остальные модули -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header>
+            <div class="wrapper">
+                <div class="header">
+                    <h1>FreeDrive</h1>
+                    <nav>
+                        <a href="#actual">Актуально</a>
+                        <a href="#about">О нас</a>
+                        <a href="#car">Автомобили</a>
+                        <a href="#rates">Тарифы</a>
+                        <a href="#application">Приложение</a>
+                        <a href="#reviews">Отзывы</a>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <section class="welcome_section">
+            <div class="wrapper">
+                <div class="ws_div">
+                    <div class="wsd_text">
+                        <h2>ПРОКАТ АВТОМОБИЛЕЙ В ГОРОДЕ ПЕНЗА</h2>
+                        <p>Будь экономным и свободным вместе с сервисом FreeDrive</p>
+                    </div>
+                    <div class="wsd_info">
+                        <div class="wsdi_text">
+                            <div>
+                                <img src="img/2.png" alt="Часы">
+                                <p>Круглосуточно</p>
+                            </div>
+                            <div>
+                                <img src="img/3.png" alt="Телефон">
+                                <p>8 800 600-34-82</p>
+                            </div>
+                        </div>
+                        <a href="#booking">НАЧАТЬ</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="about_section">
+            <div class="wrapper">
+                <div class="as_div">
+                    <h3>О нас</h3>
+                    <p>FreeDrive — это каршеринговый сервис, где быстрая и удобная аренда автомобилией разных классов на короткий срок с помощью нашего приложения.</p>
+                    <p>Находимся по адресу г. Пенза, ул. Московская, 24 Б</p>
+                    <div class="asd_items">
+                        <div>
+                            <h4>Широкий выбор авто</h4>
+                            <img src="img/4.png" alt="Машинка">
+                            <h4>От эконом класса до премиум</h4>
+                        </div>
+                        <div>
+                            <h4>Автомобили застрахованы в КАСКО</h4>
+                            <img src="img/5.png" alt="Защита">
+                            <h4>В случае ДТП, страховая компания возмещает убытки</h4>
+                        </div>
+                        <div>
+                            <h4>Поддержка по телефону</br>24 / 7</h4>
+                            <img src="img/6.png" alt="Оператор">
+                            <h4>Наши специалисты всегда готовы Вам помочь</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="car_section">
+            <div class="wrapper">
+                <div class="cs_div">
+                    <h3>Автомобили</h3>
+                    <div class="csd_filter">
+                        <a href="">Все автомобили</a>
+                        <p>/</p>
+                        <a href="">Эконом</a>
+                        <p>/</p>
+                        <a href="">Комфорт</a>
+                        <p>/</p>
+                        <a href="">Премиум</a>
+                    </div>
+                    <div class="csd_items">
+                        <!-- AJAX ответ -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="rates_section">
+            <div class="wrapper">
+                <div class="rs_div">
+                    <h3>Тарифы</h3>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
